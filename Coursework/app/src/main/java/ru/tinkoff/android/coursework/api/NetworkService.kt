5746ad5bc495a542f799ae0cf7dfffab9dde20d9
Sergey Ashihmin <serashihmin@yandex.ru>
2022-04-07 10:14:21 +0300

    hw_6: AuthInterceptor

--- Coursework/app/src/main/java/ru/tinkoff/android/coursework/api/NetworkService.kt
+++ Coursework/app/src/main/java/ru/tinkoff/android/coursework/api/NetworkService.kt
@@ -2,8 +2,10 @@ package ru.tinkoff.android.coursework.api
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -14,10 +16,9 @@ internal object NetworkService {
     internal const val AUTH_TOKEN_VALUE = "Basic c2VyYXNoaWhtaW5AeWFuZGV4LnJ1OnZ2RUJwcFRwRTVvWmg2dVZCRDJ0WEFoY05sdjl1dXlK"
     private const val BASE_URL = "https://tinkoff-android-spring-2022.zulipchat.com/"
 
-    private val loggingInterceptor = HttpLoggingInterceptor()
-
     private val okClient = OkHttpClient.Builder()
-        .addInterceptor(loggingInterceptor)
+        .addInterceptor(HttpLoggingInterceptor())
+        .addInterceptor(AuthInterceptor())
         .build()
 
     private val contentType = "application/json".toMediaType()
@@ -31,6 +32,18 @@ internal object NetworkService {
 
     fun getZulipJsonApi(): ZulipJsonApi {
         return mRetrofit.create(ZulipJsonApi::class.java)
+    }
+
+    class AuthInterceptor : Interceptor {
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request()
+            val authenticatedRequest = request.newBuilder()
+                .header(AUTH_HEADER_NAME, AUTH_TOKEN_VALUE)
+                .build()
+
+            return chain.proceed(authenticatedRequest)
+        }
     }
 
 }
--- Coursework/app/src/main/java/ru/tinkoff/android/coursework/api/ZulipJsonApi.kt
+++ Coursework/app/src/main/java/ru/tinkoff/android/coursework/api/ZulipJsonApi.kt
@@ -2,8 +2,6 @@ package ru.tinkoff.android.coursework.api
 
 import io.reactivex.Single
 import retrofit2.http.*
-import ru.tinkoff.android.coursework.api.NetworkService.AUTH_HEADER_NAME
-import ru.tinkoff.android.coursework.api.NetworkService.AUTH_TOKEN_VALUE
 import ru.tinkoff.android.coursework.model.User
 import ru.tinkoff.android.coursework.model.response.AllChannelsListResponse
 import ru.tinkoff.android.coursework.model.response.AllUsersListResponse
@@ -17,40 +15,29 @@ import ru.tinkoff.android.coursework.model.response.UserPresenceResponse
 internal interface ZulipJsonApi {
 
     @GET("api/v1/streams")
-    fun getAllStreams(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE
-    ): Single<AllChannelsListResponse>
+    fun getAllStreams(): Single<AllChannelsListResponse>
 
     @GET("api/v1/users/me/subscriptions")
-    fun getSubscribedStreams(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE
-    ): Single<SubscribedChannelsListResponse>
+    fun getSubscribedStreams(): Single<SubscribedChannelsListResponse>
 
     @GET("api/v1/users/me/{stream_id}/topics")
     fun getTopicsInStream(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE,
         @Path("stream_id") streamId: Long
     ): Single<TopicsListResponse>
 
     @GET("api/v1/users")
-    fun getAllUsers(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE
-    ): Single<AllUsersListResponse>
+    fun getAllUsers(): Single<AllUsersListResponse>
 
     @GET("api/v1/users/{user_id_or_email}/presence")
     fun getUserPresence(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE,
         @Path("user_id_or_email") userIdOrEmail: String
     ): Single<UserPresenceResponse>
 
     @GET("api/v1/users/me")
-    fun getOwnUser(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE
-    ): Single<User>
+    fun getOwnUser(): Single<User>
 
     @GET("api/v1/messages")
     fun getMessages(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE,
         @Query("num_before") numBefore: Int = 100,
         @Query("num_after") numAfter: Int = 100,
         @Query("anchor") anchor: String = "first_unread",
@@ -59,7 +46,6 @@ internal interface ZulipJsonApi {
 
     @POST("api/v1/messages")
     fun sendMessage(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE,
         @Query("type") type: String = "stream",
         @Query("to") to: String,
         @Query("content") content: String,
@@ -68,14 +54,12 @@ internal interface ZulipJsonApi {
 
     @POST("api/v1/messages/{message_id}/reactions")
     fun addReaction(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE,
         @Path("message_id") messageId: Long,
         @Query("emoji_name") emojiName: String
     ): Single<ReactionResponse>
 
     @DELETE("api/v1/messages/{message_id}/reactions")
     fun removeReaction(
-        @Header(AUTH_HEADER_NAME) authorization: String = AUTH_TOKEN_VALUE,
         @Path("message_id") messageId: Long,
         @Query("emoji_name") emojiName: String
     ): Single<ReactionResponse>
