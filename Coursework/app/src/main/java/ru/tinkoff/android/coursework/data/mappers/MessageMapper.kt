5e6d111b980ab1b3be5af83fc8e7a45af8d23cdc
Sergey Ashihmin <serashihmin@yandex.ru>
2022-05-17 22:17:05 +0300

    final_work: merge with main

--- Coursework/app/src/main/java/ru/tinkoff/android/coursework/data/mappers/MessageMapper.kt
+++ Coursework/app/src/main/java/ru/tinkoff/android/coursework/data/mappers/MessageMapper.kt
@@ -18,7 +18,7 @@ internal object MessageMapper {
     fun messagesDtoToMessagesList(messagesDto: List<MessageDto>): List<Message> =
         messagesDto.map { messageDto -> messageDtoToMessage(messageDto) }
 
-    private fun messageToMessageDb(message: Message): MessageDb {
+    fun messageToMessageDb(message: Message): MessageDb {
         return MessageDb(
             id = message.id,
             userId = message.userId,
@@ -32,7 +32,7 @@ internal object MessageMapper {
         )
     }
 
-    private fun messageDbToMessage(messageDb: MessageDb): Message {
+    fun messageDbToMessage(messageDb: MessageDb): Message {
         return Message(
             id = messageDb.id,
             userId = messageDb.userId,
