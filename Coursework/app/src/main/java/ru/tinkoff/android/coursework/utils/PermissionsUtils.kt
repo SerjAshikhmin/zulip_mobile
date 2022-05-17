889d66b23b75882cb0dd0441953dcc80ac40f487
Sergey Ashihmin <serashihmin@yandex.ru>
2022-05-17 21:47:43 +0300

    final_work: refactoring

--- Coursework/app/src/main/java/ru/tinkoff/android/coursework/utils/PermissionsUtils.kt
+++ Coursework/app/src/main/java/ru/tinkoff/android/coursework/utils/PermissionsUtils.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 
-internal fun hasPermissions(context: Context, vararg permissions: String): Boolean = permissions.all {
+internal fun hasPermissions(
+    context: Context,
+    vararg permissions: String
+): Boolean = permissions.all {
     ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
 }
